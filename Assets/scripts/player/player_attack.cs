2b33567aa7758ab8b041b99dbd6fdc4b1be614e6
terminalcrazy1 <liam.jbm2020@gmail.com>
2024-11-29 19:01:56 -0500

    Somewhat functional (Maybe)

--- Assets/scripts/player/player_attack.cs
+++ Assets/scripts/player/player_attack.cs
@@ -4,11 +4,14 @@ public class player_attack : MonoBehaviour
 {
     public GameObject weapon;
     public GameObject parent;
+    public GameObject weapon_origin;
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        Ray2D ray = new Ray2D(transform.position, parent.GetComponent<player_controller>().player_to_mouse.normalized);
+        Vector2 weapon_position = ray.GetPoint(2);
+        weapon_origin.transform.position = weapon_position;
         if (Input.GetButtonDown("Fire1")) {
-            GameObject.Instantiate(weapon, parent.transform, false);
+            GameObject.Instantiate(weapon, weapon_origin.transform, false);
         }
     }
 }
