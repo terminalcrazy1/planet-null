d3c133c15cde1c704379378c22655f012063d98d
terminalcrazy1 <liam.jbm2020@gmail.com>
2024-11-30 12:11:45 -0500

    Folder Hierarchy and Naming Conventions fixed

--- Assets/scripts/player/player_attack.cs
+++ Assets/scripts/player/player_attack.cs
@@ -8,11 +8,12 @@ public class player_attack : MonoBehaviour
     // Update is called once per frame
     void Update() {
       Debug.Log(parent.transform.position);
+      Debug.Log(weapon_origin.transform.position);
       Ray2D ray = new Ray2D(parent.transform.position, parent.GetComponent<player_controller>().player_to_mouse.normalized);
       Vector2 weapon_position = ray.GetPoint(1);
       weapon_origin.transform.position = weapon_position;
       if (Input.GetButtonDown("Fire1")) {
-        GameObject.Instantiate(weapon, weapon_origin.transform, true);
+        GameObject.Instantiate(weapon, weapon_origin.transform, false);
       }
     }
 }
