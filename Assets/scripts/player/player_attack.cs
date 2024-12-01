afe8237c28fe4feefe7238ab040d4596437fcacf
terminalcrazy1 <liam.jbm2020@gmail.com>
2024-12-01 12:50:55 -0500

    Functional rotation of weapon

--- Assets/scripts/player/player_attack.cs
+++ Assets/scripts/player/player_attack.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class player_attack : MonoBehaviour
 {
+    // Declarations
     public GameObject weapon;
     public GameObject parent;
     public GameObject weapon_origin;
+    Vector2 player_to_mouse;
+
     // Update is called once per frame
     void Update() {
-      Debug.Log(parent.transform.position);
-      Debug.Log(weapon_origin.transform.position);
-      Ray2D ray = new Ray2D(parent.transform.position, parent.GetComponent<player_controller>().player_to_mouse.normalized);
+      // Definitions
+      player_to_mouse = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - parent.transform.position);
+      Ray2D ray = new Ray2D(parent.transform.position, player_to_mouse.normalized);
       Vector2 weapon_position = ray.GetPoint(1);
       weapon_origin.transform.position = weapon_position;
+
+      // Logic
       if (Input.GetButtonDown("Fire1")) {
         GameObject.Instantiate(weapon, weapon_origin.transform, false);
       }
